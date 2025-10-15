1be94929cf7e96026ec14a2d7c3e7a2e61607d51
Muhammad Rifqi Amir Putra <muhammadrifqiamirp20@gmail.com>
2025-10-15 19:46:01 +0700

    Add JenisPerawatanSeeder to populate jenis perawatan data in the database; includes multiple oil and filter types with timestamps for created and updated records.

---
+++ app/Database/Seeds/JenisPerawatanSeeder.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class JenisPerawatanSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            [
+                'jenis_perawatan' => 'Oli Mesin',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'jenis_perawatan' => 'Oli Perseneling',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'jenis_perawatan' => 'Oli Gardan',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'jenis_perawatan' => 'Oli Power Steering',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'jenis_perawatan' => 'Pet Gemuk',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'jenis_perawatan' => 'Filter Oli',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'jenis_perawatan' => 'Filter Solar',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'jenis_perawatan' => 'Filter Bensin',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'jenis_perawatan' => 'Filter Udara',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'jenis_perawatan' => 'Minyak Rem',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ]
+        ];
+
+        $this->db->table('jenis_perawatan')->insertBatch($data);
+    }
+}
